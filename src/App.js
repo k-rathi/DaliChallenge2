1ac8a74184dd163c3c07b9e64f37da7712dd3f74
k-rathi <kunalrathi1994@gmail.com>
2018-04-30 03:32:30 -0400

    cleaned up styling and commented a bit. need to refactor.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Select from 'react-select';
 import Modal from 'react-modal';
 import Iframe from 'react-iframe';
 
+
+// standard class constructor for React in ES6.
 class App extends PureComponent {
     constructor(props) {
         super(props);
@@ -24,6 +26,10 @@ class App extends PureComponent {
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
     }
+
+    // add the json data on componentDidMount, process and save to state.
+    // Initialize the data sent to the map with all people.
+    // Attach the modal to the body.
     componentDidMount() {
         fetch(this.state.baseUrl + "members.json")
         .then(function(response) {
@@ -35,11 +41,11 @@ class App extends PureComponent {
                 Modal.setAppElement('body');
             });
     }
+
+    // Filter set method to decide who to show based on term.
     selectorSet = (selector) => {
       selector = RegExp(selector.value);
-      console.log(selector);
-      this.setState({datasent: [], selectedOption: selector.value});
-      this.setState({selector}, () => {
+      this.setState({selector, datasent: [], selectedOption: selector.value}, () => {
         let nextData = [];
         this.state.data.forEach( (person) =>  {
             for(let i = 0; i < person["terms_on"].length; i++) {
@@ -51,41 +57,69 @@ class App extends PureComponent {
         this.setState({datasent: nextData});
       });
     }
+
+    // sets a url to fetch iframe from in modal and establishes hover status.
     hover = (data) => {
-      let frameurl = (data.url.substr(0, 2) === "//" ) ?  "http://" + data.url : (data.url.substr(0, 4) === "http") ? data.url : this.state.baseUrl + data.url;
+      let frameurl = (data.url.substr(0, 2) === "//" )
+        ?  "http://" + data.url
+        : (data.url.substr(0, 4) === "http")
+          ? data.url
+          : this.state.baseUrl + data.url;
       if(this.state.currPerson.url === data.url) {
         this.setState({hoveredElement: data, toggledval: true});
         return;
       };
       this.setState({iframedata: frameurl, hoveredElement: data, currPerson: data, toggledval: true})
     }
+
+    // resets state when stop hovering element.
     unhover = (data) => {
       this.setState({hoveredElement: {}, toggledval: false})
     }
 
+    // click event for icon markers. toggles clicked state and then
     clickIcon = () => {
       this.setState({clickedElement: !this.state.clickedElement});
-      if (this.state.clickedElement)  {
-          this.openModal();
-      } else {
-        this.setState({clickedElement: false})
-      }
+      this.state.clickedElement && this.openModal();
+
     }
 
+    // open modal event
     openModal() {
         this.setState({modalIsOpen: true});
       }
 
+    // close modal event which also removes clicked, the iframeload, and current person being stored.
     closeModal() {
       this.setState({clickedElement: false, currPerson: "", iframedata: "", modalIsOpen: false});
     }
 
+    // rendering method. a bit cluttered, probably will break down into modules for pieces.
+    // Referenced http://dali.dartmouth.edu/ for favicon and DALI title image
     render = () =>
         <div className="App" >
-          <div className="title">DALI '17
-            <a className="code" href="https://github.com/k-rathi/DaliChallenge">code on github</a>
+          <div className="fixed-pos">
+            <div className="title">
+              <a width="10%" href="http://dali.dartmouth.edu/">
+                <img alt="DALI Lab" style={{width: '200px'}} src="https://bit.ly/2HE3bfy"/>
+              </a>
+              <a className="code" href="https://github.com/k-rathi/DaliChallenge2">code on github</a>
+            </div>
+            <div className="selector">
+              <Select
+                autofocus
+                name="select"
+                defaultValue='17'
+                value={this.state.selectedOption}
+                onChange={this.selectorSet}
+                options={[
+                  {value: '17W', label: '17W'},
+                  {value: '17S', label: '17S'},
+                  {value: '17', label: 'All' }
+                ]}
+              />
+            </div>
           </div>
-          <div>
           <Modal
             className="modal"
             isOpen={this.state.modalIsOpen}
@@ -98,12 +132,14 @@ class App extends PureComponent {
           >
             <div className= "flex-modal">
               <div className="flex-row">
-                <div className="thumbnail-flexed"><img alt="" className="thumb" src={`http://mappy.dali.dartmouth.edu/${this.state.currPerson.iconUrl}`}/></div>
+                <div className="thumbnail-flexed">
+                  <img alt="" className="thumb" src={`http://mappy.dali.dartmouth.edu/${this.state.currPerson.iconUrl}`}/>
+                </div>
               <div className="flex-col">
                 <div id="name">{this.state.currPerson.name}</div>
                 <div id="message">{this.state.currPerson.message}</div>
               </div>
-              <div id="projects"><div> Projects: </div> {
+              <div id="projects"><div className="prj-header"> Projects: </div> {
                 this.state.currPerson.project && this.state.currPerson.project.map((project, i) => {
                   return (<div key={i} className="prj"> {project} </div>)
                 })}
@@ -117,21 +153,7 @@ class App extends PureComponent {
               /></div>
             </div>
           </Modal>
-        </div>
-          <div className="selector">
-            <Select
-              autofocus
-              name="select"
-              defaultValue='17'
-              value={this.state.selectedOption}
-              onChange={this.selectorSet}
-              options={[
-                {value: '17W', label: '17W'},
-                {value: '17S', label: '17S'},
-                {value: '17', label: 'All' }
-              ]}
-            />
-          </div>
+
           <MyMapComponent
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjIxwKO-s7ldF5PWW0qOb1KH_BVEhLpAI"
             loadingElement={<div style={{ height: `100%` }} />}
--- src/map.jsx
+++ src/map.jsx
@@ -4,15 +4,21 @@ import { InfoWindow, withGoogleMap, GoogleMap, Marker, withScriptjs } from "reac
 import * as fancy from "./snazzymaps.json";
 
 
-export const MyMapComponent = withScriptjs(withGoogleMap((props) => {
 
-return (<GoogleMap
+// Presentational component for map
+export const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+
+// Initialize map to Dartmouth's coordinates and remove some unnecessary buttons. Style with custom google maps styling
+// using Dartmouth colors.
+<GoogleMap
   defaultOptions={{styles: fancy, mapTypeControl: false, fullscreenControl: false}}
   defaultZoom={8}
   defaultCenter={{lat: 43.7044, lng:-72.2887}}
   >
   { props.data.map( (person, i ) =>
 
+    // create all markers given the json data. Attach several propagated event handlers to change
+    // state of the container component App. Maybe too much ternary, but cleans up code so not sure.
     <Marker
       key={ i }
       clickable={ true }
@@ -21,30 +27,32 @@ return (<GoogleMap
       onMouseOver={ () => props.hover(person) }
       onMouseOut={ () => props.unhover(person) }
       title= { person.name }
-      zIndex={ person.iconUrl === props.hovered.iconUrl ?  3 : 2 }
-      shape={ person.iconUrl === props.hovered.iconUrl ? {coords: [45,45,45], type: "circle"} : {coords: [32.5,32.5,32.5], type: "circle"}}
-      icon = { person.iconUrl === props.hovered.iconUrl ? {
-          scaledSize: { width: 90, height: 90 },
-          url: `http://mappy.dali.dartmouth.edu/${person.iconUrl}`
-      } : {
-          scaledSize: { width: 65, height: 65 },
-          url: `http://mappy.dali.dartmouth.edu/${person.iconUrl}`
-      }}
+      zIndex={ person.iconUrl === props.hovered.iconUrl
+        ? 3
+        : 2 }
+      shape={ person.iconUrl === props.hovered.iconUrl
+        ? {coords: [45,45,45], type: "circle"}
+        : {coords: [32.5,32.5,32.5], type: "circle"}}
+      icon = { person.iconUrl === props.hovered.iconUrl
+        ? { scaledSize: { width: 90, height: 90 },
+            url: `http://mappy.dali.dartmouth.edu/${person.iconUrl}`}
+        : { scaledSize: { width: 65, height: 65 },
+            url: `http://mappy.dali.dartmouth.edu/${person.iconUrl}`}
+      }
       position = {{
         lat: person.lat_long[0],
         lng: person.lat_long[1]
       }}
       >
-      { person.iconUrl === props.hovered.iconUrl ?
-        <InfoWindow >
-          <div>
-            {`${person.name} : ${person.message}`}
-          </div>
-        </InfoWindow>
-      : <div></div>
+      { person.iconUrl === props.hovered.iconUrl
+        ? <InfoWindow >
+            <div>
+              {`${person.name} : ${person.message}`}
+            </div>
+          </InfoWindow>
+        : <div></div>
       }
     </Marker>
     , this)}
-</GoogleMap>)
-  }
+</GoogleMap>
 ));
